509ed8c5fd2befe805cd8786e75c8f4918474d67
Huy130296 <49013953+Huy130296@users.noreply.github.com>
2022-05-18 11:52:38 +0700

    update change log

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -9,7 +9,7 @@ from flask_login import LoginManager
 from flask_session import Session
 
 app = Flask(__name__)
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 app.config.from_object(Config)
 # TODO: Add any logging levels and handlers with app.logger
 wsgi_app = app.wsgi_app
